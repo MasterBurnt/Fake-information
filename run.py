cbed4d8869424e1f6b07364eae0cd89abce35c22
MasterBurnt <85974020+MasterBurnt@users.noreply.github.com>
2021-08-30 12:27:31 +0430

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ from colorama import Fore,init
 import os 
 import time
 
-init() 
+init()
+c1 = Fore.WHITE
+c2 = Fore.GREEN
+c3 = Fore.RED
+c4 = Fore.CYAN
+c5 = Fore.YELLOW
 os.system("clear")
 
 # api
@@ -20,7 +25,7 @@ status = True
 
 #copyright 
 def copyright():
-    print(Fore.GREEN+"""
+    print(c2+"""
                       00
                       000
                      00000                  .
@@ -44,89 +49,89 @@ def copyright():
                       000000000
                     000     000000
 """)
-    print(f"""                  F4k3 !nf0rm47!0n{Fore.WHITE} 
-                     Written by{Fore.RED}\n                    Master Burnt\n\n            {Fore.CYAN}    Telegram : @MrBurnt""")
+    print(f"""                  F4k3 !nf0rm47!0n{c1} 
+                     Written by{c3}\n                    Master Burnt\n\n            {c4}    Telegram : @MrBurnt""")
 
 
 # output
 def output():
-    print(Fore.YELLOW+"""
+    print(c5+"""
 ____ ____ _  _ ____    _ _  _ ____ ____
 |___ |__| |_/  |___    | |\ | |___ |  |
 |    |  | | \_ |___    | | \| |    |__|
 """) 
 
-    print(Fore.GREEN+f"┌─[First and last name ~@Fake information]") 
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["name"]["first"]," "+out["results"][0]["name"]["last"] )  
-    print(Fore.GREEN+"┌─[dob-date ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["dob"]["date"])
-    print(Fore.GREEN+"┌─[dob-age ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["dob"]["age"])
-    print(Fore.GREEN+"┌─[Gender ~@Fake information]") 
-    print("└──╼ 卐"+Fore.WHITE,out["results"][0]["gender"])
-    print(Fore.GREEN+"┌─[Title ~@Fake information]" )
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["name"]["title"])
-    print(Fore.GREEN+"┌─[StreetNumber ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE,out["results"][0]["location"]["street"]["number"])
-    print(Fore.GREEN+"┌─[StreetName ~@Fake information]") 
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["street"]["name"])
-    print(Fore.GREEN+"┌─[City ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["city"])
-    print(Fore.GREEN+"┌─[State ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["state"])
-    print(Fore.GREEN+"┌─[Country ~@Fake information]") 
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["country"])
-    print(Fore.GREEN+"┌─[PostCode ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["postcode"])
-    print(Fore.GREEN+"┌─[Coordinates-Latitude ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["coordinates"]["latitude"])
-    print(Fore.GREEN+"┌─[Coordinates-Longitude ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["coordinates"]["longitude"])
-    print(Fore.GREEN+"┌─[Timezone-Offset ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["timezone"]["offset"])
-    print(Fore.GREEN+"┌─[Timezone-Description ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["location"]["timezone"]["description"])    
-    print(Fore.GREEN+"┌─[Phone ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["phone"])
-    print(Fore.GREEN+"┌─[Cell ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["cell"])
-    print(Fore.GREEN+"┌─[id-name ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["id"]["name"])
-    print(Fore.GREEN+"┌─[id-value ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["id"]["value"])
-    print(Fore.GREEN+"┌─[Photo-large link ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["picture"]["large"])
-    print(Fore.GREEN+"┌─[Photo-medium link ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["picture"]["medium"])
-    print(Fore.GREEN+"┌─[Photo-large link ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["picture"]["large"])
-    print(Fore.GREEN+"┌─[Photo-thumbnail link ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["picture"]["thumbnail"])  
-    print(Fore.GREEN+"┌─[Email ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["email"]) 
-    print(Fore.GREEN+"┌─[Login-Uuid ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["uuid"])
-    print(Fore.GREEN+"┌─[Login-Username ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["username"])
-    print(Fore.GREEN+"┌─[Login-Password ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["password"])
-    print(Fore.GREEN+"┌─[Login-Salt ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["salt"])
-    print(Fore.GREEN+"┌─[Login-md5 ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["md5"])
-    print(Fore.GREEN+"┌─[Login-sha1 ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["sha1"])
-    print(Fore.GREEN+"┌─[Login-sha256 ~@Fake information]")
-    print("└──╼ 卐"+Fore.WHITE, out["results"][0]["login"]["sha256"]) 
+    print(c2+f"┌─[First and last name ~@Fake information]") 
+    print("└──╼ "+c1, out["results"][0]["name"]["first"]," "+out["results"][0]["name"]["last"] )  
+    print(c2+"┌─[dob-date ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["dob"]["date"])
+    print(c2+"┌─[dob-age ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["dob"]["age"])
+    print(c2+"┌─[Gender ~@Fake information]") 
+    print("└──╼ "+c1,out["results"][0]["gender"])
+    print(c2+"┌─[Title ~@Fake information]" )
+    print("└──╼ "+c1, out["results"][0]["name"]["title"])
+    print(c2+"┌─[StreetNumber ~@Fake information]")
+    print("└──╼ "+c1,out["results"][0]["location"]["street"]["number"])
+    print(c2+"┌─[StreetName ~@Fake information]") 
+    print("└──╼ "+c1, out["results"][0]["location"]["street"]["name"])
+    print(c2+"┌─[City ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["city"])
+    print(c2+"┌─[State ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["state"])
+    print(c2+"┌─[Country ~@Fake information]") 
+    print("└──╼ "+c1, out["results"][0]["location"]["country"])
+    print(c2+"┌─[PostCode ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["postcode"])
+    print(c2+"┌─[Coordinates-Latitude ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["coordinates"]["latitude"])
+    print(c2+"┌─[Coordinates-Longitude ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["coordinates"]["longitude"])
+    print(c2+"┌─[Timezone-Offset ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["timezone"]["offset"])
+    print(c2+"┌─[Timezone-Description ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["location"]["timezone"]["description"])    
+    print(c2+"┌─[Phone ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["phone"])
+    print(c2+"┌─[Cell ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["cell"])
+    print(c2+"┌─[id-name ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["id"]["name"])
+    print(c2+"┌─[id-value ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["id"]["value"])
+    print(c2+"┌─[Photo-large link ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["picture"]["large"])
+    print(c2+"┌─[Photo-medium link ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["picture"]["medium"])
+    print(c2+"┌─[Photo-large link ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["picture"]["large"])
+    print(c2+"┌─[Photo-thumbnail link ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["picture"]["thumbnail"])  
+    print(c2+"┌─[Email ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["email"]) 
+    print(c2+"┌─[Login-Uuid ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["uuid"])
+    print(c2+"┌─[Login-Username ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["username"])
+    print(c2+"┌─[Login-Password ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["password"])
+    print(c2+"┌─[Login-Salt ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["salt"])
+    print(c2+"┌─[Login-md5 ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["md5"])
+    print(c2+"┌─[Login-sha1 ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["sha1"])
+    print(c2+"┌─[Login-sha256 ~@Fake information]")
+    print("└──╼ "+c1, out["results"][0]["login"]["sha256"]) 
     
-    print(Fore.CYAN+" \n Information received =|\n")
+    print(c4+" \n Information received =|\n")
 
    
 
     
 
 while status:
-    goal = input(f"""{Fore.GREEN}
+    goal = input(f"""{c2}
                       00
                       000
                      00000                  .
@@ -148,17 +153,17 @@ while status:
           00000000000 00 0 0  000000000
              00000000000000 0000
                       000000000
-                    000     000000 {Fore.RED}
+                    000     000000 {c3}
 
                    F4k3 !nf0rm47!0n
                     
-{Fore.CYAN}
+{c4}
 [1] Get fake information
 [2] Written by . . .
 [3] Exit
-{Fore.GREEN}
+{c2}
 ┌─[option ~@Fake information]
-└──╼ 卐 [+] ~>{Fore.WHITE}""" )
+└──╼  [+] ~>{c1}""" )
     if goal == "1":
         os.system("clear")
         break 
@@ -171,26 +176,26 @@ while status:
             
     elif goal == "3":
         os.system("clear")
-        print(Fore.RED+"  .::Best wishes!::. ")
+        print(c3+"  .::Best wishes!::. ")
         time.sleep(1)
         os.system("clear") 
         exit(0)
         
     else:
         os.system("clear")
-        print(Fore.RED+"You only have two options: 1 or 2 or 3!Try again!" )
+        print(c3+"You only have two options: 1 or 2 or 3!Try again!" )
         time.sleep(3)
         os.system("clear")
    
 output()
 
 while status:
-        coun = input(Fore.GREEN+f"""
+        coun = input(c2+f"""
 ┌─[~$Fake information]
-└──╼ 卐{Fore.WHITE}[1] New fake information! 
-       [2] Exit!{Fore.GREEN} 
+└──╼ {c1}[1] New fake information! 
+       [2] Exit!{c2} 
 ┌─[~$Fake information]
-└──╼ 卐[+] ~> {Fore.WHITE}""")
+└──╼ [+] ~> {c1}""")
 
         if coun == "1":
             os.system("clear")
@@ -204,14 +209,14 @@ while status:
             
         elif coun == "2":
             os.system("clear")
-            print(Fore.RED+"  .::Best wishes!::. ")
+            print(c3+"  .::Best wishes!::. ")
             time.sleep(1)
             os.system("clear")
             exit(0)
             
         else:
             os.system("clear")
-            print(Fore.RED+"You only have two options: 1 or 2!Try again! " )
+            print(c3+"You only have two options: 1 or 2!Try again! " )
             time.sleep(3)
             os.system("clear")
  
